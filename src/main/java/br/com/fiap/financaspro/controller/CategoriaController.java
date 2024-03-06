1d348bf750571a0bf7d795a481d5e621cd528ccd
Kaue Santana <kauesantana_13@hotmail.com>
2024-03-05 22:45:00 -0300

    Aula 03.05

--- src/main/java/br/com/fiap/financaspro/controller/CategoriaController.java
+++ src/main/java/br/com/fiap/financaspro/controller/CategoriaController.java
@@ -2,14 +2,17 @@ package br.com.fiap.financaspro.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -51,9 +54,68 @@ public class CategoriaController {
         }
         
         // TODO Refactoring with stream
+        var categoriaEncontrada = repository
+            .stream()
+            .filter( c -> c.id().equals(id) )
+            .findFirst();
 
-        return ResponseEntity.notFound().build();//não encontrado
+        if (categoriaEncontrada.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(categoriaEncontrada.get());
 
     }
 
+    @DeleteMapping("{id}")
+    public ResponseEntity<Object> destroy(@PathVariable Long id){
+        log.info("apagando categoria");
+
+        var categoriaEncontrada = getCategoriaByid(id);
+
+        if (categoriaEncontrada.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        
+        repository.remove(categoriaEncontrada.get());
+        
+        return ResponseEntity.noContent().build();                
+    }
+
+
+    @PutMapping("{id}")
+    public ResponseEntity<Categoria> update(@PathVariable Long id, @RequestBody Categoria categoria){
+        log.info("Atualizando categoria com id {} para {}", id, categoria);
+        // Buscar a catehoria
+
+        var categoriaEncontrada = getCategoriaByid(id);
+
+        if (categoriaEncontrada.isEmpty())
+            return ResponseEntity.notFound().build();
+
+        // Criar uma nova categoria com os novos dados
+        
+        var categoriaAntiga = categoriaEncontrada.get();
+        var categoriaNova = new Categoria(id, categoria.nome(), categoria.icone());
+
+        // apagar a categoria antiga
+        repository.remove(categoriaAntiga);
+     
+       // add a categoria nova
+       repository.add(categoriaNova);
+
+       return ResponseEntity.ok(categoriaNova);
+    }
+
+
+
+    private Optional<Categoria> getCategoriaByid(Long id) {
+        var categoriaEncontrada = repository
+            .stream()
+            .filter( c -> c.id().equals(id) )
+            .findFirst();
+        return categoriaEncontrada;
+    }
+
+    
 }
