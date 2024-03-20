af5060fce69a795781cf972202dfbda515a27e49
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2024-03-19 22:45:38 -0300

    Aula 19/03

--- src/main/java/br/com/fiap/financaspro/model/Movimentacao.java
+++ src/main/java/br/com/fiap/financaspro/model/Movimentacao.java
@@ -7,6 +7,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -15,9 +20,17 @@ public class Movimentacao {
 
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotBlank(message = "Descrição é obrigatória.")
+    @Size(min = 3, max = 255, message = "Descrição deve ter pelo menos 3 caracteres.")
     private String descricao;
+
+    @Positive(message = "O valor deve ser positivo.")
     private BigDecimal valor;
+
     private LocalDate data;
+
+    // @TipoMovimentacao
     private String tipo; // Entrada ou saida
 
     
